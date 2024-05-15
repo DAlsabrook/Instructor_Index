6cb4d6067888d129ced12f9d69f2f6f4e9e0a4f4
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-05-15 03:02:01 +0000

    Adding scroll to top button

--- public/static/javascript/app.js
+++ public/static/javascript/app.js
@@ -20,6 +20,21 @@ $(document).ready(() => {
     $('#sign_up').on('click', () => {
         window.location.href = '/signup';
     });
+
+    $('#up_btn').on('click', () => {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 160 || document.documentElement.scrollTop > 160) {
+            $('#up_btn').show();
+        } else {
+            $('#up_btn').hide();
+        }
+    }
     // All buttons change to pointer hand and grow on hover
     $(document).on({
         mouseenter: function () {
